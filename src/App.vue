9ef87920caeba8768db97815d55066486d3fa794
song52wow <619202904@qq.com>
2023-08-31 12:06:09 +0800

    style: animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -21,6 +21,131 @@ const companyAnimation = ref({
 const boxAnimation = ref({
   z: -200,
 });
+const listAnimation = ref({
+  scale: 0,
+  opacity: 0,
+});
+
+const listItemAnimation = ref([{
+  scale: 0,
+  opacity: 0,
+},{
+  scale: 0,
+  opacity: 0,
+},{
+  scale: 0,
+  opacity: 0,
+},{
+  scale: 0,
+  opacity: 0,
+}]);
+
+const listImage = [
+  "https://img.alicdn.com/imgextra/i3/O1CN01TVWH501KYCEaUxPgv_!!6000000001175-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i1/O1CN01vqBxIP1L5d98G9xJD_!!6000000001248-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i3/O1CN01AKJU3T1tSi2NAFHFP_!!6000000005901-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i1/O1CN01E0GR9h1ZXwFI8dRl1_!!6000000003205-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i1/O1CN01sphiAp1Fj7bOKEppz_!!6000000000522-2-tps-480-480.png",
+  "https://img.alicdn.com/imgextra/i2/O1CN015T9BdZ28Ns5n1A82W_!!6000000007921-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i3/O1CN01bc6FKX1OCuHkDC2J7_!!6000000001670-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i1/O1CN01Y936lS1whluhvos2E_!!6000000006340-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i3/O1CN017Wk2Cp1lp8VaSd22U_!!6000000004867-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i4/O1CN016JIoXg1lMHYbmErdR_!!6000000004804-0-tps-240-240.jpg",
+  "https://img.alicdn.com/imgextra/i3/O1CN011GyvMe1of0bAveV4u_!!6000000005251-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i2/O1CN01zFSNcP26wYrM09A4S_!!6000000007726-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i2/O1CN0142F9wc23s261dItxf_!!6000000007310-0-tps-480-480.jpg",
+  "https://img.alicdn.com/imgextra/i4/O1CN012wi8vZ1xt3HbO0ttd_!!6000000006500-0-tps-480-480.jpg",
+];
+
+const listDoc = [
+  "即时沟通",
+  "组织",
+  "智能人事",
+  "组织大脑",
+  "OA审批",
+  "邮箱",
+  "Teambition",
+  "文档",
+  "音视频会议",
+  "开放平台",
+  "宜搭",
+  "钉闪会",
+  "连接器",
+  "酷应用",
+];
+
+const listStyle = computed(() => [
+  {
+    scale: listItemAnimation.value[3]["scale"],
+    opacity: listItemAnimation.value[3]["opacity"],
+    transformOrigin: 'right bottom'
+  },
+  {
+    scale: listItemAnimation.value[2]["scale"],
+    opacity: listItemAnimation.value[2]["opacity"],
+    transformOrigin: 'right bottom'
+  },
+  {
+    scale: listItemAnimation.value[1]["scale"],
+    opacity: listItemAnimation.value[1]["opacity"],
+    transformOrigin: 'right bottom'
+  },
+  {
+    scale: listItemAnimation.value[0]["scale"],
+    opacity: listItemAnimation.value[0]["opacity"],
+    transformOrigin: 'bottom'
+  },
+  {
+    scale: listItemAnimation.value[1]["scale"],
+    opacity: listItemAnimation.value[1]["opacity"],
+    transformOrigin: 'left bottom'
+  },
+  {
+    scale: listItemAnimation.value[2]["scale"],
+    opacity: listItemAnimation.value[2]["opacity"],
+    transformOrigin: 'left bottom'
+  },
+  {
+    scale: listItemAnimation.value[3]["scale"],
+    opacity: listItemAnimation.value[3]["opacity"],
+    transformOrigin: 'left bottom'
+  },
+  {
+    scale: listItemAnimation.value[3]["scale"],
+    opacity: listItemAnimation.value[3]["opacity"],
+    transformOrigin: 'right top'
+  },
+  {
+    scale: listItemAnimation.value[2]["scale"],
+    opacity: listItemAnimation.value[2]["opacity"],
+    transformOrigin: 'right top'
+  },
+  {
+    scale: listItemAnimation.value[1]["scale"],
+    opacity: listItemAnimation.value[1]["opacity"],
+    transformOrigin: 'right top'
+  },
+  {
+    scale: listItemAnimation.value[0]["scale"],
+    opacity: listItemAnimation.value[0]["opacity"],
+    transformOrigin: 'top'
+  },
+  {
+    scale: listItemAnimation.value[1]["scale"],
+    opacity: listItemAnimation.value[1]["opacity"],
+    transformOrigin: 'left top'
+  },
+  {
+    scale: listItemAnimation.value[2]["scale"],
+    opacity: listItemAnimation.value[2]["opacity"],
+    transformOrigin: 'left top'
+  },
+  {
+    scale: listItemAnimation.value[3]["scale"],
+    opacity: listItemAnimation.value[3]["opacity"],
+    transformOrigin: 'left top'
+  },
+]);
 
 const scrollTl = gsap
   .timeline()
@@ -42,13 +167,17 @@ const scrollTl = gsap
     "scroll"
   );
 
-const bannerStartTl = gsap.timeline().fromTo(companyAnimation.value, {
-  opacity: 0,
-  scale: 0,
-}, {
-  opacity: 1,
-  scale: 1,
-});
+const bannerStartTl = gsap.timeline().fromTo(
+  companyAnimation.value,
+  {
+    opacity: 0,
+    scale: 0,
+  },
+  {
+    opacity: 1,
+    scale: 1,
+  }
+);
 
 const mainStartTl = gsap
   .timeline()
@@ -85,7 +214,47 @@ const mainStartTl = gsap
       duration: 0.1,
     },
     "main+=100%"
-  );
+  )
+  .to(
+    listAnimation.value,
+    {
+      opacity: 1,
+      scale: 1,
+    },
+    "main+=50%"
+  )
+  .to(
+    listItemAnimation.value[0],
+    {
+      "scale": 1,
+      "opacity": 1,
+    },
+    "main+=80%"
+  )
+  .to(
+    listItemAnimation.value[1],
+    {
+      "scale": 1,
+      "opacity": 1,
+    },
+    "main+=70%"
+  )
+  .to(
+    listItemAnimation.value[2],
+    {
+      "scale": 1,
+      "opacity": 1,
+    },
+    "main+=60%"
+  )
+  .to(
+    listItemAnimation.value[3],
+    {
+      "scale": 1,
+      "opacity": 1,
+    },
+    "main+=50%"
+  )
 
 onMounted(() => {
   ScrollTrigger.create({
@@ -148,6 +317,30 @@ onMounted(() => {
             />
           </div>
         </div>
+        <div class="main-other">
+          <h2
+            :style="{
+              transform: `scale(${listAnimation.scale})`,
+              opacity: listAnimation.opacity,
+            }"
+          >
+            企业数字化一个钉钉就解决
+          </h2>
+
+          <ul
+            class="list"
+            :style="{
+              transform: `scale(${listAnimation.scale})`,
+              opacity: listAnimation.opacity,
+            }"
+          >
+            <li v-for="(l, i) in listImage" :key="i" :style="listStyle[i]">
+              <img :src="l" alt="" />
+
+              <p>{{ listDoc[i] }}</p>
+            </li>
+          </ul>
+        </div>
         <div class="base-bg">
           <div
             v-for="i in 21"
@@ -158,7 +351,19 @@ onMounted(() => {
           ></div>
         </div>
       </div>
-      <div class="main-other"></div>
+      <!-- <div class="main-other">
+        <h2>企业数字化一个钉钉就解决</h2>
+
+        <ul class="list" :style="{
+          transform: `scale(${listAnimation.scale})`
+        }">
+          <li v-for="(l, i) in listImage" :key="i">
+            <img :src="l" alt="">
+
+            <p>{{ listDoc[i] }}</p>
+          </li>
+        </ul>
+      </div> -->
     </section>
   </div>
   <section class="footer"></section>
@@ -277,7 +482,11 @@ $baseStyle: (
     left: calc(50% - 1.54em),
     top: calc(50% - 4.73em),
     bg: (
-      linear-gradient(-27deg, rgba(143, 53, 255, 0.8) 11%, rgba(143, 53, 255, 0)),
+      linear-gradient(
+        -27deg,
+        rgba(143, 53, 255, 0.8) 11%,
+        rgba(143, 53, 255, 0)
+      ),
       radial-gradient(
         circle at 62% -28%,
         rgba(0, 45, 156, 0.75) 0,
@@ -449,11 +658,15 @@ $baseStyle: (
 .main {
   background-color: #040506;
   overflow: hidden;
+  font-size: 100px;
+  color: #ffffff;
 
   .main-animation {
     font-size: 100px;
-    height: 200vh;
+    height: 100vh;
     position: relative;
+    display: flex;
+    align-items: center;
 
     .company {
       text-align: center;
@@ -463,6 +676,8 @@ $baseStyle: (
       align-items: center;
       justify-content: center;
       flex-direction: column;
+      position: absolute;
+      top: 0;
 
       .logo {
         width: 180px;
@@ -497,7 +712,34 @@ $baseStyle: (
   }
 
   .main-other {
-    height: 400px;
+    width: 12em;
+    margin: 0 auto;
+    text-align: center;
+    border-radius: .16em;
+
+    h2 {
+      font-size: 0.48em;
+    }
+
+    .list {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      list-style: none;
+      row-gap: 1.3em;
+      background-color: rgba(23,26,29,.9);
+      padding: .83em .43em;
+      box-sizing: border-box;
+
+      img {
+        border-radius: 0.1667em;
+        height: 0.8em;
+        width: 0.8em;
+      }
+
+      p {
+        font-size: 0.2em;
+      }
+    }
   }
 }
 </style>
